28001e090695ba6cb790b14e630bd06479673a20
mra <tad)goW>
2019-02-22 07:18:49 +0100

    use su, not sudo

--- bootstrap-salt.sh
+++ bootstrap-salt.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+# https://raw.githubusercontent.com/m-rau/appliance/master/bootstrap-salt.sh
+
 cd
 test -d .pnbi_salt || mkdir .pnbi_salt
 cd .pnbi_salt
 wget -O bootstrap-salt.sh https://bootstrap.saltstack.com
-sudo sh bootstrap-salt.sh
+su -u root sh bootstrap-salt.sh
