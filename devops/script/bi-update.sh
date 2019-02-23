20facf43c9189648845431cbc44324ad4560d22b
mra <tad)goW>
2019-02-24 00:20:54 +0100

    wip: check upgrade

---
+++ devops/script/bi-update.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+# Check return value to see if there are incoming updates.
+if ! git diff --quiet remotes/origin/HEAD; then
+ # pull or whatever you want to do
+ echo "have to pull"
+fi
+
+UPSTREAM=${1:-'@{u}'}
+LOCAL=$(git rev-parse @)
+REMOTE=$(git rev-parse "$UPSTREAM")
+BASE=$(git merge-base @ "$UPSTREAM")
+
+if [ $LOCAL = $REMOTE ]; then
+    echo "Up-to-date"
+elif [ $LOCAL = $BASE ]; then
+    echo "Need to pull"
+elif [ $REMOTE = $BASE ]; then
+    echo "Need to push"
+else
+    echo "Diverged"
+fi
