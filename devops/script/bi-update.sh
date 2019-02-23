f4295aa97b5485f08b0692d380c3143d8080ac21
mra <tad)goW>
2019-02-24 00:34:08 +0100

    start real update workflow

--- devops/script/bi-update.sh
+++ devops/script/bi-update.sh
@@ -1,23 +1,7 @@
 #!/bin/sh
 
-git status -uno
-# Check return value to see if there are incoming updates.
-if ! git diff --quiet remotes/origin/HEAD; then
- # pull or whatever you want to do
- echo "have to pull"
-fi
-
-UPSTREAM=${1:-'@{u}'}
-LOCAL=$(git rev-parse @)
-REMOTE=$(git rev-parse "$UPSTREAM")
-BASE=$(git merge-base @ "$UPSTREAM")
-
-if [ $LOCAL = $REMOTE ]; then
-    echo "Up-to-date"
-elif [ $LOCAL = $BASE ]; then
-    echo "Need to pull"
-elif [ $REMOTE = $BASE ]; then
-    echo "Need to push"
+if [ "`git fetch --dry-run`" != "" ]; then
+    echo "need to update"
 else
-    echo "Diverged"
+    echo "all up-to-date"
 fi
