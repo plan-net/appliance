63bc7582eaa992d91c60d86c5aa1da71fb199587
mra <tad)goW>
2019-02-24 00:54:45 +0100

    upgrade

--- bi-update.py
+++ bi-update.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 from subprocess import check_output, check_call, STDOUT
-from os.path import expanduser, abspath, join
+from os.path import expanduser, abspath, join, exists
 from os import chdir, system
 import sys
 
@@ -10,21 +10,20 @@ worktree = join(home, ".pnbi_salt/appliance")
 chdir(worktree)
 out = check_output([
     "git", "fetch", "--dry-run"], stderr=STDOUT).decode("utf-8").strip()
-if out == "":
-    print("no upgrade required")
-    sys.exit(0)
-print("Plan.Net Business Intelligence")
-print("=> your devops station requires update")
-while True:
-    print("do you want to upgrade now [y/n]: ", end="")
-    inp = input().lower().strip()
-    if inp == "y":
-        break
-    elif inp == "n":
-        sys.exit(1)
+if out != "" or exists(".upgrade"):
+    print("Plan.Net Business Intelligence")
+    print("=> your devops station requires update")
+    while True:
+        print("do you want to upgrade now [y/n]: ", end="")
+        inp = input().lower().strip()
+        if inp == "y":
+            open(".upgrade", "w").write("")
+            break
+        elif inp == "n":
+            sys.exit(1)
 
-print("run upgrade")
-check_call(["git", "pull"])
-
-cmd = "sudo salt-call --file-root {}/devops -l debug --local state.apply setup | tee {}/salt_call.log".format(worktree, home)
-system(cmd)
+if exists(".upgrade"):
+    print("run upgrade")
+    check_call(["git", "pull"])
+    cmd = "sudo salt-call --file-root {}/devops -l debug --local state.apply setup | tee {}/salt_call.log".format(worktree, home)
+    system(cmd)
