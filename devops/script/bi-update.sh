a9a9e64374e932533fb3eb8fde383b6f15edb557
Michael Rau <m.rau@plan-net.com>
2021-01-13 21:34:55 +0100

    fix $USER variable

--- devops/script/bi-update.sh
+++ devops/script/bi-update.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-touch "/home/$SUDO_USER/.pnbi_salt/.upgrade"
-"/home/$SUDO_USER/.pnbi_salt/update-bi.py"
+touch "/home/$USER/.pnbi_salt/.upgrade"
+"/home/$USER/.pnbi_salt/update-bi.py"
